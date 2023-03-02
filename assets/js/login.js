82a718f9327e940206e87b83ce164d8244362e3f
mcthekreator <mcadamsrd@gmail.com>
2023-03-02 21:04:24 +0000

    initial commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,31 +1,29 @@
-function login() {
+// function login() {
 
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-
-  if (username === "" || password === "") {
-    alert("Please enter your username and password");
-    return false;
-  }
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
 
 
+//   if (username === "" || password === "") {
+//     alert("Please enter your username and password");
+//     return false;                                                                                                                                                                                                                                          
+//   }
  
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "/authenticate", true);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open("POST", "/authenticate", true);
+//   xhr.setRequestHeader("Content-type", "application/json");
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
 
-      window.location.href = "/home";
-    } else if (xhr.readyState === 4 && xhr.status === 401) {
+//       window.location.href = "/home";
+//     } else if (xhr.readyState === 4 && xhr.status === 401) {
 
-      alert("Invalid username or password");
-    }
-  };
-  const data = JSON.stringify({ username: username, password: password });
-  xhr.send(data);
-}
+//       alert("Invalid username or password");
+//     }
+//   };
+//   const data = JSON.stringify({ username: username, password: password });
+//   xhr.send(data);
+// }
 
 // ========Login=========
 const loginForm = document.getElementById("loginForm")
